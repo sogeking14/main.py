a5029c5210f2efb966c688fa22a2b51d4757855f
sogeking14 <patax5698@mail.ru>
2024-07-22 10:11:19 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+#  "1st program"
+print(9 ** 0.5 * 5)
+
+
+# "2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+
+# "3nd program"
+print((1234%1000//10)+(5678%1000//10))
+# "4nd program"
+a = 13.42
+b = 42.13
+a1= int(a)
+b1= int((a*100)%100)
+a2= int(b)
+b2=int((b*100)%100)
+print(a1==b2 or a2==b1)
